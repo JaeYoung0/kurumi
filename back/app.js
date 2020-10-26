70bb83ca308e83351d3683d939646f34795f6855
JaeYoungHeo <hjy8697@gmail.com>
2020-10-26 16:53:04 +0900

    cors /

--- back/app.js
+++ back/app.js
@@ -37,7 +37,7 @@ app.use(helmet())
 // cors피해가기 위해서 cors미들웨어 장착
 app.use(
   cors({
-    origin: ["https://ipapi.co/json","http://kurum2.com/","13.209.153.169/"], //나는 3065이긴한데 3060들어갈 수 있게해줘
+    origin: ["https://ipapi.co/json","http://kurum2.com","13.209.153.169"], //나는 3065이긴한데 3060들어갈 수 있게해줘
     credentials: true, //도메인 달라도 쿠키 전달하게 해줌
     methods:
     "GET,HEAD,PUT,PATCH,POST,DELETE",   
