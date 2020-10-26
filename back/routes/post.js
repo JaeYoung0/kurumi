44b6a584d979fbc1bc92271372fcf4a1bdbb63cc
JaeYoungHeo <hjy8697@gmail.com>
2020-10-26 23:06:15 +0900

    .env fix as Se->S3

--- back/routes/post.js
+++ back/routes/post.js
@@ -19,7 +19,7 @@ try {
 
 // S3
 AWS.config.update({
-  accessKeyId: process.env.SE_ACCESS_KEY_ID,
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
   region:'ap-northeast-2', 
 })
