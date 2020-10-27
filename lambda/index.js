88d0ddd32d105754eb51b8209bc0babb4be4672c
JaeYoungHeo <hjy8697@gmail.com>
2020-10-27 22:40:07 +0900

    cors

--- lambda/index.js
+++ lambda/index.js
@@ -7,13 +7,13 @@ exports.handler = async(event, context, callback)=>{
     const Key = decodeURIComponent(event.Records[0].s3.object.key) ; // original/123123123_abc.png
     console.log(Bucket, Key)
     const filename = Key.split('/')[Key.split('/').length - 1];
-    const ext = Key.split('.') [Key.split('.').length - 1];
+    const ext = Key.split('.') [Key.split('.').length - 1].toLowerCase();
     const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
-
+    console.log('filename', filename, 'ext', ext)
 
 try{
     const s3Object = await s3.getObject({Bucket, Key}).promise();
-    console.log('original', s3Object.Body.lenght)
+    console.log('original', s3Object.Body.length)
     const resizedImage = await sharp(s3Object.Body)
     .resize(400, 400, {fit:'inside'})
     .toFormat(requiredFormat)
