1ba1a897f2a55ea70db5c30cfc2eb36d0115776b
JaeYoungHeo <hjy8697@gmail.com>
2021-01-24 16:35:08 +0900

    fix: 404 custom 이미지

--- front/pages/404.js
+++ front/pages/404.js
@@ -1,9 +1,39 @@
+import styled from 'styled-components'
+import {Button} from 'antd'
+import {useRouter} from 'next/router'
+
+const Wrapper = styled.img`
+/* display: flex;
+justify-content: center;
+align-items: center; */
+/* width: 1000px;
+@media (max-width: 1024px) {
+    width: 300px
+  } */
+`
+
 export default function Custom404(){
+    const router = useRouter();
     return(
-        <div style={{display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
-            <h1 style={{marginTop:'20px', fontWeight:'bold'}}>404 NOT FOUND!</h1>
-            <h1 style={{marginTop:'20px', fontWeight:'bold'}}>개발 중인 페이지입니다.</h1>
-            <img src="/profile_kurumi1.png"/>
+        <>
+        <div style={{
+            height: '100vh', 
+        width: '100vw', 
+        display:'flex', 
+        alignItems:'center', 
+        justifyContent:'center',
+        flexDirection:'column'
+        }}>
+            <img src="/images/404kurumi.png" style={{width:'70vw', maxWidth:'400px', minWidth:'200px'}} />
+            <Button
+            style={{
+                marginTop: '20px',
+                border: '1px solid #3585BE',
+                color: '#3585BE'
+                }}
+                onClick={()=>{router.push('/')}}
+            >Home</Button>
         </div>
+        </>
     )
 }
